2483463b6043f426157fdd3d13fbc6661a192395
Gabriel <costanzag1999@gmail.com>
2023-02-14 11:40:52 +0100

    Working on search tags

--- src/infrastructure/tags/controllers/tags.controller.ts
+++ src/infrastructure/tags/controllers/tags.controller.ts
@@ -1,10 +1,11 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { createTagDto } from '../dto/create-tag.dto';
 import { TagCreateUseCase } from 'src/application/tags/tag-create.use-case';
 import { Tag } from 'src/domain/tags/tag';
 import { Auth } from 'src/infrastructure/users/decorators/auth.decorator';
 import { GetUser } from 'src/infrastructure/users/decorators/get-user.decorator';
 import { Professor } from 'src/domain/users/professor';
+import { PaginationDto } from '../../dto/pagination.dto';
 
 @Controller('tags')
 export class TagsController {
@@ -19,5 +20,10 @@ export class TagsController {
         const tag = new Tag();
         tag.setDataFromInt(createTagDto);
         return this.tagCreateUseCase.execute(tag, professor);
+    }
+
+    @Get()
+    search(@Query() paginationDto: PaginationDto) {
+        // const { term = '', limit = 10, offset = 0 } = paginationDto;
     }
 }
