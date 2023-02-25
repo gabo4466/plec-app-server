8d33df7a101e826ab372c53f9e3d389bfd5e63a2
Gabriel <costanzag1999@gmail.com>
2023-02-25 22:47:36 +0100

    Firebase admin

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+// TODO: CARGAR EL ARCHIVO DE CONFIGURACIÓN DE FIREBASE DESDE EL .ENV
+
 async function bootstrap() {
+    console.log(JSON.parse(process.env.FIREBASE_ADMIN_CREDENTIALS));
+
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(
         new ValidationPipe({
