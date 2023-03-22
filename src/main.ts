5561f3e91ff5d571fab604a25dbabb042f9d8515
Gabriel <costanzag1999@gmail.com>
2023-03-22 11:27:48 +0100

    CORS policy

--- src/main.ts
+++ src/main.ts
@@ -25,6 +25,7 @@ async function bootstrap() {
     });
 
     const app = await NestFactory.create(AppModule);
+    app.enableCors();
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
